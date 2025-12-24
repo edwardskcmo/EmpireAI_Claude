9e12bf7f984205f63031cb254ad118c83c567c65
edwardskcmo <134224824+edwardskcmo@users.noreply.github.com>
2025-12-24 12:23:22 -0600

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+---
+
+## File 9: `.gitignore`
+
+**Filename:** `.gitignore` (in root)
+```
+node_modules
+dist
+.env
+.env.local
+*.log
+.DS_Store
